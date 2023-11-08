b4fed9f0a16d060c4671560d009debc421a9b349
S.Zelensky <zelenstein@gmail.com>
2023-11-08 22:53:27 +0300

    decomp config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,28 @@
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+import { buildLoaders } from './buildLoaders';
+import { buildPlugins } from './buildPlugins';
+import { buildResolvers } from './buildResolvers';
+
+export function buildWebpackConfig(
+  options: BuildOptions
+): webpack.Configuration {
+
+  const {paths, mode} = options;
+
+  return {
+    mode,
+    entry: paths.entry,
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+    output: {
+      filename: '[name].[contenthash].js',
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    devtool: 'inline-source-map',
+  };
+}
