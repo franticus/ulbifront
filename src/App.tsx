ce416f477f2eda93cdaa60a6922193996db09c2b
S.Zelensky <zelenstein@gmail.com>
2023-11-15 20:39:27 +0300

    async

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Counter } from './components/Counter';
 import './index.scss';
+import { Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { AboutPageAsync } from 'src/pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from 'src/pages/MainPage/MainPage.async';
 
 export const App = () => {
   return (
     <div className='app'>
-      Hello World!
-      <Counter />
+      <Link to='/'>Главная</Link>
+      <Link to='/about'>О сайте</Link>
+      <Routes>
+        <Route path='/about' element={<AboutPageAsync />} />
+        <Route path='/' element={<MainPageAsync />} />
+      </Routes>
     </div>
   );
 };
