45a2daf13c684f842b291123c61d785bf076efb5
S.Zelensky <zelenstein@gmail.com>
2023-11-15 20:58:40 +0300

    lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import { Suspense } from 'react';
 import './index.scss';
 import { Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { AboutPageAsync } from 'src/pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from 'src/pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 export const App = () => {
   return (
     <div className='app'>
       <Link to='/'>Главная</Link>
       <Link to='/about'>О сайте</Link>
-      <Routes>
-        <Route path='/about' element={<AboutPageAsync />} />
-        <Route path='/' element={<MainPageAsync />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/about' element={<AboutPageAsync />} />
+          <Route path='/' element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
